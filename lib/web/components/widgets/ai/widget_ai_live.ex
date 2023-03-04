017e159167bd2ae9ffc22a26471aae427da949da
ivanminutillo <ivanminutillo@outlook.it>
2023-03-04 10:39:43 +0100

    wip

---
+++ lib/web/components/widgets/ai/widget_ai_live.ex
@@ -0,0 +1,4 @@
+defmodule Bonfire.AI.Web.WidgetAILive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+end
---
+++ lib/web/components/widgets/nooverse/widget_nooverse_live.ex
@@ -0,0 +1,51 @@
+defmodule Bonfire.AI.Web.WidgetNooverseLive do
+  use Bonfire.UI.Common.Web, :stateful_component
+
+  prop text, :string, default: nil
+  prop prediction, :string, default: nil
+  prop running?, :boolean, default: false
+  prop serving, :any, default: nil
+  prop label, :string, default: nil
+
+  def update(assigns, socket) do
+    {:ok,
+    socket
+    |> assign(assigns)
+    |> assign(running?: false)
+    |> assign(text: nil)
+    |> assign(label: "nil")
+    |> assign(prediction: nil)
+    |> assign(serving: serving())
+    |> assign(text: "I am so happy today!")}
+  end
+
+  def handle_event("start", _params, socket) do
+    send(self(), :run)
+
+    {:noreply, assign(socket, running?: true)}
+  end
+
+  def handle_info(:run, %{assigns: %{running?: true}} = socket) do
+    {:ok, prediction} = predict(socket.assigns.serving, socket.assigns.text)
+  end
+
+  def handle_info(_msg, socket), do: {:noreply, socket}
+
+
+  defp predict(serving, text) do
+    res = Nx.Serving.run(serving, text)
+  end
+
+  defp serving do
+    {:ok, model_info} = Bumblebee.load_model({:hf, "finiteautomata/bertweet-base-emotion-analysis"})
+    {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, "vinai/bertweet-base"})
+
+    Bumblebee.Text.text_classification(model_info, tokenizer,
+      top_k: 1,
+      compile: [batch_size: 10, sequence_length: 100],
+      defn_options: [compiler: EXLA]
+    )
+
+  end
+
+end
